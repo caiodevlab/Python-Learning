dfd8b2dffe8b9584fad56952ecea5994c03e0224
Caiodevlab <hcaio0741@gmail.com>
2026-08-16 14:51:07 -0300

    Refatorarei a exibição do menu e o tratamento de entrada no programa de calculadora.

--- python-learning/03 - loops/exercicios/ex059.py
+++ python-learning/03 - loops/exercicios/ex059.py
@@ -1,6 +1,34 @@
 valor1 = int(input('primeiro valor: '))
 valor2 = int(input('Segundo valor: '))
 acao = 0
+menu = ' [1]somar\n [2]multiplicar\n [3]maior\n [4]novos numeros\n [5]sair do programa'
+print(menu)
+acao = int(input('Oque deseja fazer:'))
 while acao != 5:
-    print('MENU')
-    print('')
+    if acao == 1:
+        print(valor1 + valor2)
+        print(menu)
+        acao = int(input('Oque deseja fazer:'))
+    if acao == 2:
+        print(valor1 * valor2)
+        print(menu)
+        acao = int(input('Oque deseja fazer:'))
+
+    if acao == 3:
+        if valor1 > valor2:
+            print(f'Primeiro valor é maior {valor1}')
+            print(menu)
+            acao = int(input('Oque deseja fazer:'))
+
+        else:
+            print(f'Segundo valor é maior {valor2}')
+            print(menu)
+            acao = int(input('Oque deseja fazer:'))
+    if acao == 4:
+        print('Inserir novamente')
+        valor1 = int(input('Primeiro valor: '))
+        valor2 = int(input('Segundo valor: '))
+        print(menu)
+        acao = int(input('Oque deseja fazer: '))
+    if acao == 5:
+        print('Tenha um bom dia')
