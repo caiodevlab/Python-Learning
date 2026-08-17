fd1eda553137dbc89e3114dfc7fd5ab8d5256f55
Caiodevlab <hcaio0741@gmail.com>
2026-08-17 10:47:27 -0300

    Adiciona implementação de um loop para cálculo de sequência numérica

---
+++ python-learning/03 - loops/exercicios/Repetiçoes em python(Whyle)/063.py
@@ -0,0 +1,11 @@
+numero = int(input('Numero inteiro: '))
+n = 0
+n2 = 1
+while n != 10:
+    numero += n2
+    n2 += 1
+    n + n2
+    n = n2
+    n2 = n + n2
+    print(numero, n2)
+    #falta arrumar
