b7172ced5b424bf456ad65143377cd6debfe08c2
Caiodevlab <hcaio0741@gmail.com>
2026-08-17 10:20:59 -0300

    Adiciona exercícios de repetição em Python com validação de entrada e cálculo de fatorial

--- python-learning/03 - loops/exercicios/Repetiçoes em python(Whyle)/ex061.py
+++
@@ -1,9 +0,0 @@
-primeiroterm = int(input('Primeiro termo: '))
-razao = int(input('Razão: '))
-termo = primeiroterm
-cont = 0
-while cont != 10:
-    termo += razao
-    cont += 1
-    print(f'{cont}° termo = {termo}')
-print('FIM')
